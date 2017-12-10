92f434826adad0524177a7e2b422411ab5270471
Alumno <alumno@Rojo.local>
2017-12-09 20:56:55 -0600

    Add flexslider, Youtube, Facebook, Twitter, Maps

--- ejercicio1/js/main.js
+++ ejercicio1/js/main.js
@@ -1,4 +1,18 @@
-$ (function() {
-    var navPage = $ ('#navPage');
-    navPage.sticky({topSpacing:0});
+$(function () {
+    //Variables
+    var navPage = $('#navPage');
+    var slider=$('#slider')
+
+    //Apply plugins
+    navPage.sticky({ topSpacing: 0 });
+    slider.flexslider();
 });
+
+// SDK Facebook 
+(function (d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.11';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
